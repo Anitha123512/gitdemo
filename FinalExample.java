12cf4d2728f4f270e539339b70f086f102007016
Anitha_123 <krsani.msc@gmail.com>
2021-01-04 15:33:03 +0530

    FinalExample.java files

---
+++ FinalExample.java
@@ -0,0 +1,7 @@
+public class FinalExample {
+    public static void main(String[] args) {
+        final int x=100;
+        //int x=200;//compile time error
+        System.out.println(x);
+    }
+}
