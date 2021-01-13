7192f042aa3100406430b2d07efeb949847d8a54
Anitha_123 <krsani.msc@gmail.com>
2021-01-13 22:49:56 +0530

    Employee1.java files

---
+++ Employee1.java
@@ -0,0 +1,45 @@
+ package com.example.springproject;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+    @JsonRootName(value= "employee")
+    @JsonPropertyOrder({"id", "name"})
+    public class Employee1 {
+        @JsonProperty("idByname")
+        int id;
+        String name;
+        public Employee1() {
+        }
+
+        public Employee1(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
