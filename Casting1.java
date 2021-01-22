466c1575721c704cfe2bc9614abb03ae2c80736e
Anitha_123 <krsani.msc@gmail.com>
2021-01-22 22:08:10 +0530

    java files

---
+++ Casting1.java
@@ -0,0 +1,8 @@
+public class Casting1 {
+    public static void main(String[] args) {
+        double myDouble=9.78;
+        int myInt=(int) myDouble;
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+}
