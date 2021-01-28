72d446ebbb3ab24cca1794d5d7eec87d867552dd
Anitha_123 <krsani.msc@gmail.com>
2021-01-28 16:40:33 +0530

    java files

---
+++ Wrapper2.java
@@ -0,0 +1,16 @@
+public class Wrapper2 {
+        public static void main(String[] args) {
+            String s="10";
+            int i=Integer.parseInt(s);
+            Integer x=Integer.parseInt(s);
+            System.out.println("i= "+i);
+            System.out.println("x= "+x);
+
+            String s1="10";
+            double d=Double.parseDouble(s1);
+            Double x1=Double.parseDouble(s1);
+            System.out.println("d= "+d);
+            System.out.println("x1= "+x1);
+
+        }
+    }
