21f47ec8524ae1abb9323af83755c33d830f509e
Anitha_123 <krsani.msc@gmail.com>
2021-01-28 16:37:53 +0530

    java files

---
+++ Color.java
@@ -0,0 +1,10 @@
+public enum Color {
+        RED, GREEN, BLUE;
+
+        // Driver method
+        public static void main(String[] args)
+        {
+            Color c1 = Color.RED;
+            System.out.println(c1);
+        }
+    }
