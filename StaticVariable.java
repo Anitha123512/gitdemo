6edd158597d3cc49876b8d7fb625a9f433bdb011
Anitha_123 <krsani.msc@gmail.com>
2021-02-10 11:40:13 +0530

    java files

---
+++ StaticVariable.java
@@ -0,0 +1,14 @@
+public class StaticVariable{
+    static int a=10;
+    static int b=20;
+    /*static {
+        a = 100;
+        b = 200;
+    }*/
+    public static void main(String args[]) {
+
+        System.out.println("Value of a = " + a);
+        System.out.println("Value of b = " + b);
+
+    }
+}
