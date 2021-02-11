dd4bc38cc12562868a024a788254c0d0f317a2da
Anitha_123 <krsani.msc@gmail.com>
2021-02-11 16:35:47 +0530

    java files

---
+++ StringMethods.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String x="Anitha";
+        System.out.println(x.length());
+        System.out.println(x.toUpperCase());
+        System.out.println(x.toLowerCase());
+        String y="anitha is good girl";
+        System.out.println(y.indexOf("is"));
+        System.out.println(y.indexOf("good"));
+    }
+}
