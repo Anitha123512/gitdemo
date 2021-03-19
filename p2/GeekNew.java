1f56d8b42abbba865d550a484f7022814d993b19
Anitha_123 <krsani.msc@gmail.com>
2021-03-19 15:22:29 +0530

    package

---
+++ p2/GeekNew.java
@@ -0,0 +1,24 @@
+
+// Java program to illustrate error while
+// using class from different package with
+// default modifier
+package p2;
+import p1.*;
+
+// This class is having default access modifier
+class GeekNew
+{
+    public static void main(String args[])
+    {
+        // Accessing class Geek from package p1
+        Geek obj = new Geek();
+
+        obj.display();
+    }
+}
+class GeekNews extends GeekNew{
+    public static void main(String[] args) {
+        Geek m=new Geek();
+        m.display();
+    }
+}
